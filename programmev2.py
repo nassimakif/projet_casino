f4b3506d2db19f6bdad8d3b47a409b55becdeb49
JeanTalgornThomas <51022157+JeanTalgornThomas@users.noreply.github.com>
2020-11-17 12:48:38 +0100

    Recommencer en cas d'échec + Affichage

--- programmev2.py
+++ programmev2.py
@@ -9,22 +9,28 @@ def checkSaisiNombre(nb_user, max):
 
 #Saisie de début
 name_user = str(input("Je suis Python. Quel est votre pseudo ? "))
-print("Hello " + name_user + ", vous avez 10 €, Très bien ! Installez vous SVP à la table de pari.\n Je vous expliquerai le principe du jeu : ")
+print("Hello " + name_user + ", vous avez 10 €, Très bien ! Installez vous SVP à la table de pari.\n\nJe vous expliquerai le principe du jeu ! ")
 argent = 10
 level = 0
+nb_user = 0
 
 while level < 4:
-    if level == 0:
-        print("1 - Level 1")
-        print("4 - Sortir");
-    elif level < 4:
-        print("Super ! Vous passez au Level " + str(level+1));
-        print(str(level+1) + " - Pour passer au level " + str(level+1))
-        print("4 - Sortir");
-    elif level == 4:
-        print("4 - Sortir");
-    
-    level = int(input("Que voulez-vous faire : "))
+    if nb_user != "erreur":
+        if level == 0:
+            print("\t1 - Level 1")
+            print("\t4 - Sortir");
+        elif level < 4:
+            print("\n\n Super ! Vous passez au Level " + str(level+1));
+            print("\t" + str(level+1) + " - Pour passer au level " + str(level+1))
+            print("\t4 - Sortir");
+        elif level == 4:
+            print("\t4 - Sortir");
+        level = int(input("Que voulez-vous faire : "))
+    else:
+        print("\n\n Vous avez perdu au niveau " + str(level))
+        print("\t" + str(level) + " - Pour recommencer au level " + str(level))
+        print("\t4 - Sortir")
+        level = int(input("Que voulez-vous faire : "))
 
     if level < 4:
         if level == 1:
@@ -42,39 +48,39 @@ while level < 4:
         print("Nombre mystere = " + str(nb_python))
 
         #Régle
-        print("Je viens de penser à un nombre entre 1 et " + str(max) + ". Devinez lequel ? \n Att : vous avez le droit à trois essais !")
-        print("\n Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !")
-        print("\n Si vous le devinez au 2è coup, vous gagnez exactement votre mise !")
-        print("\n Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !")
-        print("\n Si vous ne le devinez pas au 3è coup, vous perdez votre mise et \n vous avez le droit :")
-        print("\n \t de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu")
-        print("\n \t de quitter le jeu")
-        print("\n Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur.")
+        print("\n\nJe viens de penser à un nombre entre 1 et " + str(max) + ". Devinez lequel ? \nAtt : vous avez le droit à trois essais !")
+        print("Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !")
+        print("Si vous le devinez au 2è coup, vous gagnez exactement votre mise !")
+        print("Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !")
+        print("Si vous ne le devinez pas au 3è coup, vous perdez votre mise et \nVous avez le droit :")
+        print("\t- de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu")
+        print("\t- de quitter le jeu")
+        print("Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur.")
 
         #Montant de la mise
         if level == 1:
-            mise = int(input("Le jeu commence, entrez votre mise : "))
+            mise = int(input("\n\nLe jeu commence, entrez votre mise : "))
         else:
-            mise = int(input("Entrez votre mise : "))
+            mise = int(input("\n\nEntrez votre mise : "))
             
         while mise < 1 or mise > argent:
-            print("Le montant saisi n'est pas valide.")
+            print("Erreur, votre mise est plus elevé que votre solde.")
             mise = int(input("Entrer SVP un montant entre 1 et " + argent + " € : "))
 
         #Nombre mystère
-        nb_user = int(input("Alors mon nombre est : "))
+        nb_user = int(input("\n\nAlors mon nombre est : "))
         nb_user = checkSaisiNombre(nb_user, max)
         nb_coup = 1;
         while nb_user != nb_python or nb_user == "erreur":
             if nb_user < nb_python:
-                nb_user = int(input("Votre nbre est trop petit ! \n Alors mon nombre est : "))
+                nb_user = int(input("\nVotre nbre est trop petit ! \n Alors mon nombre est : "))
                 nb_user = checkSaisiNombre(nb_user, max)
             if nb_user > nb_python:
-                nb_user = int(input("Votre nbre est trop grand ! \n Alors mon nombre est : "))
+                nb_user = int(input("\nVotre nbre est trop grand ! \n Alors mon nombre est : "))
                 nb_user = checkSaisiNombre(nb_user, max)
             nb_coup += 1;
             if nb_coup == nb_essai-1 and nb_user != nb_python:
-                print("Il vous reste une chance !")
+                print("\nIl vous reste une chance !")
             if nb_coup == nb_essai and nb_user != nb_python:
                 nb_user = "erreur"
                 break;
