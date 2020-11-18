4b0c0c759615789eef4c426cdc831d5bca04424c
JeanTalgornThomas <51022157+JeanTalgornThomas@users.noreply.github.com>
2020-11-18 16:55:29 +0100

    Gestion d'erreur temps de réponse

--- programmev2.py
+++ programmev2.py
@@ -1,8 +1,5 @@
 #Module
 from random import *
-import sys
-import select
-import os.path
 from os import path
 from inputimeout import inputimeout, TimeoutOccurred
 
@@ -16,13 +13,29 @@ def checkSaisiNombre(nb_user, max):
     return nb_user
 
 def rules():
-    print("Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !")
-    print("Si vous le devinez au 2è coup, vous gagnez exactement votre mise !")
-    print("Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !")
-    print("Si vous ne le devinez pas au 3è coup, vous perdez votre mise et \nVous avez le droit :")
-    print("\t- de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu")
-    print("\t- de quitter le jeu")
-    print("Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur.")
+    regles = input("Voulez-vous voir les règles ? (o ou n) : ")
+    if regles == 'o' or regles == 'O':
+        print("Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !")
+        print("Si vous le devinez au 2è coup, vous gagnez exactement votre mise !")
+        print("Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !")
+        print("Si vous ne le devinez pas au 3è coup, vous perdez votre mise et \nVous avez le droit :")
+        print("\t- de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu")
+        print("\t- de quitter le jeu")
+        print("Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur.")
+    elif regles == 'n' or regles =='N':
+        print("Bonne chance à vous... ne pas lire les règles peut être préjudiciable....")
+    else:
+        print("Je n'ai pas compris votre input")
+
+def mise(argent):
+    if level == 1:
+        mise = int(input("\n\nLe jeu commence, entrez votre mise : "))
+    else:
+        mise = int(input("\n\nEntrez votre mise : "))
+    while mise < 1 or mise > argent:
+        print("Erreur, votre mise est plus elevé que votre solde.")
+        mise = int(input("Entrer SVP un montant entre 1 et " + str(argent) + " € : "))
+    return mise
 
 #Saisie de début
 name_user = str(input("Je suis Python. Quel est votre pseudo ? "))
@@ -44,10 +57,10 @@ while level < 4:
             print("\n\n Super ! Vous passez au Level " + str(level))
             print("\t" + str(level) + " - Pour passer au level " + str(level))
             print("\t4 - Sortir")
-            levelaffichage = level
+            levelprecedent = level
             level = int(input("Que voulez-vous faire : "))
-            while levelaffichage != level and level != 4 : 
-                level = int(input("Réponse incorect, veuillez saisir le choix " + str(levelaffichage) + " ou 4 ! "))
+            while levelprecedent != level and level != 4 : 
+                level = int(input("Réponse incorect, veuillez saisir le choix " + str(levelprecedent) + " ou 4 ! "))
         elif level == 3:
             level = 4
     else:
@@ -75,49 +88,45 @@ while level < 4:
         print("Nombre mystere = " + str(nb_python))
 
         #Regle
-        regles = input("Voulez-vous voir les règles ? \n o ou n\n")
-        if regles == 'o' or regles == 'O':
-            rules()
-            
-        elif regles == 'n' or regles =='N':
-            print("Bonne chance à vous... ne pas lire les règles peut être préjudiciable....")
-        else:
-            print("Je n'ai pas compris votre input")
-        print("\n\nJe viens de penser à un nombre entre 1 et " + str(max) + ". Devinez lequel ? \nAtt : vous avez le droit à trois essais !")
+        rules()
+        print("\n\nJe viens de penser à un nombre entre 1 et " + str(max) + ". Devinez lequel ? \nAtt : vous avez le droit à " + str(nb_essai) + " essais !")
         
         #Montant de la mise
-        if level == 1:
-            mise = int(input("\n\nLe jeu commence, entrez votre mise : "))
-        else:
-            mise = int(input("\n\nEntrez votre mise : "))
-            
-        while mise < 1 or mise > argent:
-            print("Erreur, votre mise est plus elevé que votre solde.")
-            mise = int(input("Entrer SVP un montant entre 1 et " + str(argent) + " € : "))
+        mise = mise(argent)
 
         #Nombre mystere
         try:
             nb_user = inputimeout(prompt='(10 secondes pour répondre) \n\nAlors mon nombre est : ', timeout=10)
         except TimeoutOccurred:
             print("Vous n'avez pas répondu assez vite")
-        nb_user = checkSaisiNombre(int(nb_user), max)
+        if nb_user != 0:
+            nb_user = checkSaisiNombre(int(nb_user), max)
         nb_coup = 1
         while nb_user != nb_python or nb_user == "erreur":
             if nb_user < nb_python:
                 try:
-                    nb_user = inputimeout(prompt=' (10 secondes pour répondre)  \nVotre nbre est trop petit ! \n Alors mon nombre est : ', timeout=10)
+                    if nb_user != 0:
+                        print("\nVotre nombre est trop petit !")
+                    nb_user = inputimeout(prompt=' (10 secondes pour répondre) \n Alors mon nombre est : ', timeout=10)
                 except TimeoutOccurred:
                     print("Vous n'avez pas répondu assez vite")
-                nb_user = checkSaisiNombre(int(nb_user), max)
+                if nb_user != 0:
+                    nb_user = checkSaisiNombre(int(nb_user), max)
             if nb_user > nb_python:
                 try:
-                    nb_user = inputimeout(prompt='(10 secondes pour répondre) \nVotre nbre est trop grand ! \n Alors mon nombre est : ', timeout=10)
+                    if nb_user != 0:
+                        print("\nVotre nombre est trop grand !")
+                    nb_user = inputimeout(prompt='(10 secondes pour répondre) \n Alors mon nombre est : ', timeout=10)
                 except TimeoutOccurred:
                     print("Vous n'avez pas répondu assez vite")
-                nb_user = checkSaisiNombre(int(nb_user), max)
+                if nb_user != 0:
+                    nb_user = checkSaisiNombre(int(nb_user), max)
             nb_coup += 1
             if nb_coup == nb_essai-1 and nb_user != nb_python:
                 print("\nIl vous reste une chance !")
+            if nb_user == 0:
+                nb_user = "erreur"
+                break
             if nb_coup == nb_essai and nb_user != nb_python:
                 nb_user = "erreur"
                 break
