da074014f5a2e446ed03103be3c39f727007b7ce
nassimakif <nassim.akif94@gmail.com>
2020-11-20 16:20:27 +0100

    update final pour soutenance ... !

--- casino.py
+++ casino.py
@@ -10,11 +10,12 @@ from check import Check
 from level import Level
 from stats import Stats
 from mise import Mise
-
+from datetime import datetime
 #Saisie de début
 Stats.creation_stats()
 name_user = str(input("Je suis Python. Quel est votre pseudo ? "))
 argent = Check.checkInt("Combien voulez-vous mettre dans le jeu : ")
+print("\nHello " + name_user + ", vous avez " + str(argent) + " €, Très bien ! Installez vous SVP à la table de pari.\n\nJe vous expliquerai le principe du jeu ! ")
 level = 0
 nb_user = 0
 start = True
@@ -22,6 +23,8 @@ meilleure_gain = 0
 pire_gain = 0
 meilleure_mise = 0
 pire_mise = 0
+gain = 0
+mise = 0
 
 while level < 4:
     if nb_user != "erreur":
@@ -35,7 +38,7 @@ while level < 4:
     else:
         level = Level.niveau_perdu(level)
         if level != 4 and argent == 0: 
-            argent = Check.checkInt("\nVous avez tout perdu. Combien voulez-vous remettre ? : ")
+            argent = int(input("\nVous avez tout perdu. Combien voulez-vous remettre ? : "))
     
     if level < 4:
         limiteMaxi = 0
@@ -49,18 +52,15 @@ while level < 4:
         elif level == 3:
             limiteMaxi = 30
             nb_essai = 7
-
         #Generation nombre
         nb_python = randint(1, limiteMaxi)
         print("Nombre mystere = " + str(nb_python))
-
         #Regle
         Rules.rules()
         print("\nJe viens de penser à un nombre entre 1 et " + str(limiteMaxi) + ". Devinez lequel ? \nAttention : vous avez le droit à " + str(nb_essai) + " essais !")
         
         #Montant de la mise
         mise = Mise.fonction_mise(argent, level)
-
         #Nombre mystere
         nb_coup = 1
         if nb_user == "erreur":
@@ -70,8 +70,12 @@ while level < 4:
         except TimeoutOccurred:
             print("Vous n'avez pas répondu assez vite")
         if nb_user != 0:
-            nb_user = Check.checkSaisiNombre(int(nb_user), limiteMaxi)
-        while int(nb_user) != nb_python:
+            try:
+                nb_user = Check.checkSaisiNombre(int(nb_user), limiteMaxi)
+            except ValueError:
+                print("Veuillez entrer un nombre")
+        nb_coup = 1
+        while int(nb_user) != nb_python or int(nb_user) == "erreur":
             if int(nb_user) < nb_python:
                 nb_user = Check.check_trouver_nombre("Votre nombre est trop petit !", int(nb_user))
             if int(nb_user) > nb_python:
@@ -79,13 +83,12 @@ while level < 4:
             nb_coup += 1
             if nb_coup == nb_essai-1 and nb_user != nb_python:
                 print("\nIl vous reste une chance !")
-            if nb_coup == 0:
+            if nb_user == 0:
                 nb_user = "erreur"
                 break
             if nb_coup == nb_essai and nb_user != nb_python:
                 nb_user = "erreur"
                 break
-
         #Resultat
         if nb_user == "erreur":
             print("\nVous avez perdu ! Mon nombre est " + str(nb_python) + " !")
@@ -99,7 +102,6 @@ while level < 4:
                 pire_gain = gain
             meilleure_gain = Stats.meilleureGain(gain, meilleure_gain)
             pire_gain = Stats.pireGain(gain, pire_gain)
-
         if level == 1:
             meilleure_mise = mise
             pire_mise = mise
@@ -107,10 +109,21 @@ while level < 4:
         pire_mise = Stats.pireMise(mise, pire_mise)
     else:
         if level == 4:
-            Stats.save_stats(meilleure_gain, pire_gain, meilleure_mise, pire_mise, name_user)
-
-        print("\nAu revoir ! Vous finissez la partie avec " + str(argent) + " €")
-        print("\nVotre meilleur gain est " + str(meilleure_gain)  + " €" )
-        print("Votre meilleur mise est " + str(meilleure_mise)  + " €")
-        print("Votre pire gain est " + str(pire_gain)  + " €")
-        print("Votre pire mise est " + str(pire_mise) + " €")
+            date = str(datetime.now())
+            l = date.split('.')
+            date = str(l[0])   
+            Stats.save_stats(meilleure_gain, pire_gain, meilleure_mise, pire_mise, name_user, date)
+            
+        data_json = Stats.dataJson("stats.json")
+        lMeilleure_gain = data_json[name_user]['meilleure gain']
+        lMeilleure_mise = data_json[name_user]['meilleure mise']   
+        lPire_gain = data_json[name_user]['pire gain']
+        lPire_mise = data_json[name_user]['pire mise']
+        ldate = data_json[name_user]['date']
+        print("\nAu revoir ! Vous finissez la partie avec " + str(argent + gain - mise) + " €")
+        print("\nVous avez joué {} partie(s) dans notre CASINO".format(len(data_json[name_user]['meilleure gain'])))
+        print("\nVoici vos stats de la partie du " + ldate[lPire_gain.index(min(lPire_gain))])
+        print("\n\nVotre meilleur gain est " + str(max(lMeilleure_gain)) + "euros")
+        print("\nVotre meilleur mise est " + str(max(lMeilleure_mise)) + "euros")
+        print("\nVotre pire gain est " + str(min(lPire_gain)) + "euros")
+        print("\nVotre pire mise est " + str(min(lPire_mise)) + "euros")
--- stats.py
+++ stats.py
@@ -2,7 +2,6 @@ import json
 from json.decoder import JSONDecodeError
 from os import path
 import os.path
-
 class Stats:
     def __init__(self, gain, meilleure_gain, pire_gain, mise, meilleur_mise, pire_mise, name_user):
         self.gain = gain
@@ -12,14 +11,12 @@ class Stats:
         self.meilleur_mise = meilleur_mise
         self.pire_mise = pire_mise
         self.name_user = name_user
-
     # retourne les meilleures gains (meilleures gains) pour les stats joueurs
     def meilleureGain(gain, meilleure_gain):
         if gain > meilleure_gain:
             return gain
         else:
             return meilleure_gain
-
     # retourne les meilleures mises (meilleures mises) pour les stats joueurs
     def meilleureMise(mise, meilleure_mise):
         if mise > meilleure_mise:
@@ -32,20 +29,19 @@ class Stats:
             return pire_mise
         else:
             return mise
-
     # retourne les pire gains (gains les plus faibles) pour les stats joueurs
     def pireGain(gain, pire_gain):
         if pire_gain < gain:
             return pire_gain
         else:
             return gain
-
-    def save_stats(meilleure_gain, pire_gain, meilleure_mise, pire_mise, name_user):
+    def save_stats(meilleure_gain, pire_gain, meilleure_mise, pire_mise, name_user, date):
         donnees = { name_user : {
         'meilleure gain':[meilleure_gain],
         'pire gain': [pire_gain],
         'meilleure mise': [meilleure_mise],
-        'pire mise': [pire_mise]
+        'pire mise': [pire_mise],
+        'date' : [date]
         }
         }
         with open("stats.json", "r") as file:
@@ -57,12 +53,18 @@ class Stats:
                     data_json[name_user]['pire gain'].append(pire_gain)
                     data_json[name_user]['meilleure mise'].append(meilleure_mise)
                     data_json[name_user]['pire mise'].append(pire_mise)
+                    data_json[name_user]['date'].append(date)
                     json.dump(data_json, file)
                 else:
                     data_json["list_pseudo"].append(name_user)
                     data_json.update(donnees)
                     json.dump(data_json, file)
     
+    def dataJson(JsonFile):
+        with open("stats.json", "r") as file:
+            data_json = json.load(file)
+            file.close()
+            return data_json
     def creation_stats():
         stat = {
                 "list_pseudo" : []
